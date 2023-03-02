15fd52523286cec9018e2f89f6b367bbaf41c8ec
PradeepR-proponent <pradeep.r@proponenttechnologies.com>
2023-03-02 13:14:27 +0530

    section added

---
+++ Components/Mainbanner.vue
@@ -0,0 +1,174 @@
+<template>
+    <section class="hero-wrap">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8">
+                    <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-bs-interval="10000">
+      <img src="../assets/img/slider/slider-1.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item" data-bs-interval="2000">
+      <img src="../assets/img/slider/slider-1.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="../assets/img/slider/slider-1.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+
+
+
+                    <!-- <div class="owl-carousel owl-theme owl-loaded" id="slider">
+                        <div class="owl-stage-outer">
+                            <div class="owl-stage">
+                                <div class="owl-item">
+                                    <img src="../assets/img/slider/slider-1.jpg" class="img-fluid" alt="">
+                                </div>
+                                <div class="owl-item">
+                                    <img src="../assets/img/slider/slider-1.jpg" class="img-fluid" alt="">
+                                </div>
+                                <div class="owl-item">
+                                    <img src="../assets/img/slider/slider-1.jpg" class="img-fluid" alt="">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="owl-dots"></div>
+                    </div> -->
+                </div>
+                <div class="col-lg-4">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="aside-right">
+                                <div class="row">
+                                    <div class="col-md-4 d-flex align-items-center">
+                                        <img src="../assets/img/slider/rubber.png" class="img-fluid" alt="">
+                                    </div>
+                                    <div class="col-md-8">
+                                        <h3 class="heading">NATURAL RUBBER SVRL 3L</h3>
+                                        <P class="text">SVRL 3L rubber (equivalent to TSR 3L technical rubber)</P>
+                                        <a href="" class="detail-link">+ Get Detail</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="aside-right">
+                                <div class="row">
+                                    <div class="col-md-4 d-flex align-items-center">
+                                        <img src="../assets/img/slider/wet-wipes.png" class="img-fluid" alt="">
+                                    </div>
+                                    <div class="col-md-8">
+                                        <h3 class="heading">Wet wipes</h3>
+                                        <P class="text">Baby care wet wipes 80 sheets do not use Phenoxyethanol preservatives, banned Parabens.</P>
+                                        <a href="" class="detail-link">+ Get Detail</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row border my-4 mx-0">
+                <div class="col-lg-2">
+                    <a href="">
+                        <div class="product-wrap">
+                            <img src="../assets/img/icons/byuer-leads/medical.png" class="img-fluid" alt="">
+                            <span class="product-name">Medical Supplies</span>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-2">
+                    <a href="">
+                        <div class="product-wrap">
+                            <img src="../assets/img/icons/byuer-leads/food.png" class="img-fluid" alt="">
+                            <span class="product-name">Food & Agriculture</span>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-2">
+                    <a href="">
+                        <div class="product-wrap">
+                            <img src="../assets/img/icons/byuer-leads/industrial.png" class="img-fluid" alt="">
+                            <span class="product-name">Industrial Products</span>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-2">
+                    <a href="">
+                        <div class="product-wrap">
+                            <img src="../assets/img/icons/byuer-leads/fashion.png" class="img-fluid" alt="">
+                            <span class="product-name">Fashion & Accessories</span>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-2">
+                    <a href="">
+                        <div class="product-wrap">
+                            <img src="../assets/img/icons/byuer-leads/cosmetic.png" class="img-fluid" alt="">
+                            <span class="product-name">Cosmetic, Personal Care</span>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-2">
+                    <a href="">
+                        <div class="product-wrap">
+                            <img src="../assets/img/icons/byuer-leads/furniture.png" class="img-fluid" alt="">
+                            <span class="product-name">Furniture & Appliance</span>
+                        </div>
+                    </a>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col">
+                    <div class="box-wraper">
+                        <div class="icons-box">
+                            <img src="../assets/img/icons/sg-support.png" alt="" class="img-fluid">
+                            <h2 class="heading">sgs support</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="box-wraper">
+                        <div class="icons-box">
+                            <img src="../assets/img/icons/tuv-support.png" class="img-fluid" alt="">
+                            <h2 class="heading">TUV SUPPORT</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="box-wraper">
+                        <div class="icons-box">
+                            <img src="../assets/img/icons/safe-trade.png" class="img-fluid" alt="">
+                            <h2 class="heading">SAFE TRADE</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="box-wraper">
+                        <div class="icons-box">
+                            <img src="../assets/img/icons/verified-supplier.png" class="img-fluid" alt="">
+                            <h2 class="heading">VERIFIED SUPPLIER</h2>
+                        </div>
+                    </div>
+                </div>                
+                <div class="col">
+                    <div class="box-wraper">
+                        <div class="icons-box">
+                            <img src="../assets/img/icons/globalnetwork.png" class="img-fluid" alt="">
+                            <h2 class="heading">GLOBAL NETWORK</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
